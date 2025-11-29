7e741a47a7193277efe05482284e6e4d1153c332
Andrew Wright <135736975+Astrodrew2@users.noreply.github.com>
2025-11-29 17:45:00 -0500

    Update MagiconomyApp.py

--- MagiconomyApp.py
+++ MagiconomyApp.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import matplotlib.pyplot as plt
 from atomwords import draw_atom_words_from_dict  # your plotting function
+import glyphdict
+import modsdict
 
 # Example data – replace with your actual dictionaries
 words_dict = glyphdict.words_dict
