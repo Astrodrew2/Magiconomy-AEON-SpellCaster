3a640f96ae97990973d380c9d0645ff2b7add37b
Andrew Wright <135736975+Astrodrew2@users.noreply.github.com>
2025-12-06 15:50:31 -0500

    Update modsdict.py

--- modsdict.py
+++ modsdict.py
@@ -5,8 +5,8 @@ mod_dict = {
     #Ley
      "Shoot Ice1": {"shape":"square","size":2, "appto":["Shoot Ice EN"], "AP":1, "energy":0, "comment": "Ice deals d6 × 2 (Piercing) PD ⚔️/2 PP,(Foe rolls 1 medium AGI checks, if from point or beam, 2 medium AGI checks if cone, and 3 medium AGI checks if radial Glyph of Ice), Add 1 AP Requirement: 20 ft range, 16 INT, and Master (If not, DEATH)"},
     "Shoot Ice2": {"shape":"double_square", "size":2, "appto":["Shoot Ice EN"], "AP":3, "energy":0, "comment": "Ice deals 100 (Piercing) ⚔️/6 PP Add 3 AP, Requirement: Ice with weight >=3, 10 ft range, 16 INT, and Master (If not ALL, DEATH)"},
-     "Ley Fire Burn": {"shape":"circle","size":1,"appto":["Flame","Greater Flame"], "AP":5, "energy":0, "comment": "Burn + d12 x INT MD✨ (Force), Add 5 AP.Requirement: 16 INT and Master (If not, DEATH) "},
+     "Ley Fire Damage": {"shape":"circle","size":1,"appto":["Flame","Greater Flame"], "AP":5, "energy":0, "comment": "Burn + d12 x INT MD✨ (Force), Add 5 AP.Requirement: 16 INT and Master (If not, DEATH) "},
     #End 
-    "Crystal Proj": {"shape":"diamond","size":2, "appto":["Crystal"], "AP":0, "energy":1, "comment": "Crystal or shards send toward target (if character this hits all limbs), shooting D4 projectiles (only 1 if single Crystal) dealing d10 (Piercing) ⚔️/1 PP (for shards) or dealing d10 (Force)⚔️"}
+    "Crystal Projectile": {"shape":"diamond","size":2, "appto":["Crystal"], "AP":0, "energy":1, "comment": "Crystal or shards shoot toward a target. For shards this shoots D4 projectiles (this hits all limbs) dealing d10 (Piercing) ⚔️/1 PP. For a single crystal it deals d10 (Force)⚔️, to one limb"}
 
 }
