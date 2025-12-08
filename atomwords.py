fb42c79aff9c4cd3728d2d96b5d9a2de55400d50
Andrew Wright <135736975+Astrodrew2@users.noreply.github.com>
2025-12-07 20:54:45 -0500

    Update atomwords.py

--- atomwords.py
+++ atomwords.py
@@ -146,7 +146,7 @@ def draw_electron_connection(ax, p1, p2, n_lines=1, spacing=0.15, sec1=None, sec
         perp /= np.linalg.norm(perp)
     
         # V size
-        V_size = 1.5
+        V_size = 2
     
         p_left  = arrow_pos + perp * V_size
         p_right = arrow_pos - perp * V_size
