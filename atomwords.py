555e68cea67bc6e31fc5d8ebbffca396ac0e6e8f
Andrew Wright <135736975+Astrodrew2@users.noreply.github.com>
2026-01-24 21:43:59 -0500

    Update atomwords.py

--- atomwords.py
+++ atomwords.py
@@ -404,7 +404,7 @@ def draw_modifier_shape(ax, shape, center, size=1.0, color='black', level=1):
         ax.plot(corners[:, 0], corners[:, 1], corners[:, 2], color=color, linewidth=1.5,zorder = 13)
         ax.plot(corners2[:, 0], corners2[:, 1], corners2[:, 2], color=color, linewidth=1.5,zorder = 13)
     elif shape == "triangle":
-        half = size*1.5 
+        half = size*2 
         y_offset = half * 0.35*1.5 
         corners = np.array([
             [x0,        y0 - half- y_offset+0.2, z],        # top
