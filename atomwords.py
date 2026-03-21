535bbfc529128189eaa7424705a4416df2f0d041
Andrew Wright <135736975+Astrodrew2@users.noreply.github.com>
2026-03-21 16:30:51 -0700

    Update atomwords.py

--- atomwords.py
+++ atomwords.py
@@ -1073,7 +1073,7 @@ def draw_atom_words_from_dict(words_list, words_dict, modifiers_dict=None, modif
         #print(tabulate(table, headers=["From","To","Base AP","Base Energy","Cross AP","Cross Energy",
                                        #"Domain","Range","RT","Comment","Chan","AP"], tablefmt="fancy_grid"))
         df = pd.DataFrame(table,columns=["From","To","Base AP","Base Energy","Cross AP","Cross Energy","Domain","Range","RT","Comment","Chan","AP"])
-        print("EFFECTS:",df["Comment"])
+        print("EFFECTS:",df["Comment"].to_string(index=False))
     
     
         printed_output = buffer.getvalue()
