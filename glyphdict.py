3219387d10f2cbb66ef92bd5ac0fc70507af3329
Andrew Wright <135736975+Astrodrew2@users.noreply.github.com>
2026-03-26 18:38:58 -0700

    Update glyphdict.py

--- glyphdict.py
+++ glyphdict.py
@@ -32,7 +32,7 @@ words_dict = {
     "Greater Flame": {"mastery":"Master","level":2,"section":6,"AP":1, "range":30, "rt": 4, "comment": "Energize the Ley winds you cast upon with far greater power and control making that which it touches burn and effect the natural world around it. Damage: If caught in flames, 2 x INT MD✨and Causes Burn.", "chann":1, "book":1 },
     "Greater Chill": {"mastery":"Master","level":4,"section":6,"AP":1, "range":30, "rt": 5, "comment": "Instant, The Ley winds caught within your cast come to stop briefly inflicting Inflict 6 Slow on targets within your range", "chann":2, "book":1 },
     #Enhancements: Novice
-    "Brightness EN": {"mastery":"Novice","level":1,"section":6,"AP":1, "range":0, "rt": 0, "comment": "adds stun effect to Glyph of Light for targets within 10ft", "chann":2 },
+    "Brightness EN": {"mastery":"Novice","level":1,"section":6,"AP":1, "range":0, "rt": 0, "comment": "adds stun effect to Glyph of Light for targets within 10ft", "chann":2, "book":1},
     #Enhancements: Adept
     "Vertical EN": {"mastery":"Adept","level":1,"section":6,"AP":1, "range":0, "rt": 0, "comment": "Adds 6ft to the height of the channeled or long-lasting glyph attached (mass is increased as would be expected). Duration: long-lasting glyph", "chann":2, "book":1 },
     "Shoot Ice EN": {"mastery":"Adept","level":1,"section":6,"AP":1, "range":0, "rt": 0, "comment": "If the caster chooses to release the Ley ice as it pulls away from them during formation it shoots their Ley Ice up to the Glyph of Ice’s specified range (Unless modified with Shoot Ice1 or 2). Only usable with w<=2 Ice", "chann":2, "book":1 },
